f1ba7fe14ee814a4833a9114adbf665daa88f5fa
Jonathan Keeping <Jonathan.Keeping@nice.com>
2022-06-12 16:00:33 +0100

    Section 6 complete

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: packageJson.dependencies // Share all the package.json dependencies
+    }),
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
+
+
+
