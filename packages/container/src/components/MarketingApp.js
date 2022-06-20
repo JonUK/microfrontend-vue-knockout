dc2786a9d7fa2027e989b1e597d4aaa685af5139
Jonathan Keeping <Jonathan.Keeping@nice.com>
2022-06-20 14:25:43 +0100

    Section 6 complete - Added parent and child routing
    Using browser router for the container and memory router for the child packages

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,25 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          console.log('onNavigate called from marketing app', nextPathname);
+          history.push(nextPathname);
+        }
+      }
+    });
+
+    history.listen(onParentNavigate);
+  }, []); // Only run the once on initial render
 
   return <div ref={ref} />;
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        console.log('onParentNavigate called from container app', nextPathname);
+        history.push(nextPathname);
+      }
+    }
+  };
 }
 
 // If we are in development and in isolation then call mount immediately
@@ -11,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
