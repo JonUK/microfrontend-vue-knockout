727154c89c2cbb6e81c55a5fb9674c31787ec860
Jonathan Keeping <Jonathan.Keeping@nice.com>
2022-06-20 20:43:41 +0100

    Lazy load apps and use linear loader for progress

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp'
+
+import Progress from './components/Progress';
 import Header from './components/Header';
-import AuthApp from './components/AuthApp'
+// import AuthApp from './components/AuthApp';
+// import MarketingApp from './components/MarketingApp';
 
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -16,10 +21,13 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+
         </div>
       </StylesProvider>
     </BrowserRouter>
